ad745b40ea7b3dcc1a7605fff5dceedd0582d494
kishorepamulapati <45882741+kishorepamulapati@users.noreply.github.com>
2019-01-25 02:27:43 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "tf-helloworld-kishore"
-    key    = "tf-backend"
+    bucket = "ebiz-helloworld-kishore"
+    key    = "s3-backend"
     region = "us-east-1"
   }
 }
