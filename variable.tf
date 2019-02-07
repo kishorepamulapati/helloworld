3502d66de50339827c92f96f3aa9dc2dcc60765f
kishorepamulapati <45882741+kishorepamulapati@users.noreply.github.com>
2019-02-08 00:03:56 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,9 +1,8 @@
-variable "instance-ami" {
-}
+variable "instance_ami" {
+ }
 variable "subnet-id" {
-  default = "subnet-48050911"
+  default = "subnet-0fd17e31"
 }
 variable "vpc-id"{
   default ="vpc-6052e104"
 }
-
