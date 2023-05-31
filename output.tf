c6f49f928b167b5f36d74c92cd7f8a90198996ad
kishorepamulapati <45882741+kishorepamulapati@users.noreply.github.com>
2023-05-31 11:45:33 +0530

    Delete output.tf

--- output.tf
+++
@@ -1,9 +0,0 @@
-output "instance_ami" {
-  value = "${aws_instance.EC2.id}"
-}
-output "sg-elb" {
- value = "${aws_security_group.SG-ELB.id}"
-}
-output "elb-dns" {
-  value = "${aws_elb.ELB.dns_name}"
-}
